b9e1cbf86003c6e8c6955c3121417080b09911fa
Alexander Hoffmann <alexander.hoffmann@artificial-solutions.com>
2019-08-02 17:28:59 +0800

    fixed the port

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 let orchestrationLayer = require("./orchestrationLayer.js");
 let singleUseToken = require("./singleUseToken.js");
 
-const {
-	SINGLEUSETOKEN_PORT,
-  OLAYER_PORT
-} = process.env;
+singleUseToken.startServer(3030);
+orchestrationLayer.startServer(process.env.PORT || 3000);
 
-
-singleUseToken.startServer(SINGLEUSETOKEN_PORT || 3030);
-orchestrationLayer.startServer(OLAYER_PORT || 3000);
-
-console.log("Now running on "+OLAYER_PORT);
+console.log("Now running on "+process.env.PORT);
