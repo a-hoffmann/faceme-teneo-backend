eb821fdb877d935ecb51a386baa47a01987ce3da
a-hoffmann <alexandr.hoffmann@gmail.com>
2019-11-12 08:52:07 +0800

    disabled token process, unused

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let orchestrationLayer = require("./orchestrationLayer.js");
-let singleUseToken = require("./singleUseToken.js");
+/*let singleUseToken = require("./singleUseToken.js");
+singleUseToken.startServer(3030);*/
 
-singleUseToken.startServer(3030);
 orchestrationLayer.startServer(process.env.PORT || 3000);
 
 console.log("Now running on "+(process.env.PORT || 3000));
